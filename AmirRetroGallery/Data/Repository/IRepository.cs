bf98911d04980be1cc47b9f4f82b26d5b7f65c0a
amir <amirsdream@gmail.com>
2021-03-01 00:04:33 +0100

    Add project files.

---
+++ AmirRetroGallery/Data/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using AmirRetroGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmirRetroGallery.Data.Repository
+{
+    public interface IRepository
+    {
+        Post GetPost(int id);
+        List<Post> GetAllPost();
+        void AddPost(Post post);
+        void UpdatePost(Post post);
+        void RemovePost(int id);
+
+        Task<bool> SaveChangesAsync();
+    }
+}
---
+++ AmirRetroGallery/Data/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using AmirRetroGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmirRetroGallery.Data.Repository
+{
+    public class Repository : IRepository
+    {
+
+        private AppDbContext _ctx;
+
+        public Repository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public Post GetPost(int id)
+        {
+            return _ctx.Posts.FirstOrDefault(p => p.Id == id);
+        }
+        public List<Post> GetAllPost()
+        {
+            return _ctx.Posts.ToList();
+        }
+        public void AddPost(Post post)
+        {
+            _ctx.Posts.Add(post);
+            
+        }
+        public void UpdatePost(Post post)
+        {
+            _ctx.Posts.Update(post);
+        }
+        public void RemovePost(int id)
+        {
+            _ctx.Posts.Remove(GetPost(id));
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            if (await _ctx.SaveChangesAsync() > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
